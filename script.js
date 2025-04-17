e42d70f63441570e5efd569ab559fcef0df57093
ailintorrente <ailu.torrente@gmail.com>
2025-04-17 20:56:12 -0300

    Cambios

---
+++ script.js
@@ -0,0 +1,10 @@
+/*const navMenu = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition >= 110) { // Adjust the threshold as needed
+    navMenu.classList.add("navFadeIn"); // Apply a new class for a fade-in effect
+  } else {
+    navMenu.classList.remove("navFadIn");
+  }
+});*/
