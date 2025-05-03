de73dd522d5ba6de4eb4b74c50e125277e5f29de
ailintorrente <ailu.torrente@gmail.com>
2025-05-02 23:03:41 -0300

    Entrega Final

--- scriptHistoria.js
+++ scriptHistoria.js
@@ -1,12 +1,11 @@
 /*CONTROLES VIDEO PLAY PAUSE*/
 let videoHistoria = document.querySelector("#videoHistoria")
-
-function iniciarVideo(){
+/* function iniciarVideo(){
   videoHistoria.play()
 }
 function pausarVideo(){
   videoHistoria.pause()
-}
+} */
 const BodyHistoria=()=>{
     if(window.scrollY > 50){
         document.querySelector("nav").classList.add("scroll");
@@ -27,3 +26,33 @@ const BodyHistoria=()=>{
         document.querySelector("#textoHistoria3").style="animation: animationHistoria_textoDerecha 1.5s forwards linear;";
     }
 }
+
+let transformarTiempoActual=(tiempo)=>{
+  if(tiempo<60){
+      if(tiempo.toFixed(0)<10){
+          return `00:0${tiempo.toFixed(0)}`
+      }
+      return `00:${tiempo.toFixed(0)}`
+      //metodo toFixed(cant. decimales)
+  }else{
+      console.log(tiempo/60 )
+      let minutos=parseInt(tiempo/60) //-> cantidad de minutos, solo tomo el valor entero
+      let segundos= (tiempo/60 - minutos)*60
+      if(segundos<10){
+          return `${minutos}:0${segundos.toFixed(0)}`
+      }
+      return `${minutos}:${segundos.toFixed(0)}`
+  }
+  //
+}
+let tiempoActual;
+function iniciarVideo(){
+  videoHistoria.play();
+  tiempoActual=setInterval(()=>{
+  document.querySelector("#textDuracionVideo").innerHTML=`${transformarTiempoActual(videoHistoria.currentTime)}`
+},1000);
+};
+function pausarVideo(){
+  videoHistoria.pause();
+  clearInterval(tiempoActual);
+};
